d300edcd6caf17d736fe932aa3290e3c1cb4c1f2
bcoffield <empty@example.com>
2022-02-10 14:01:06 -0700

    init with nextjs

---
+++ components/Header.js
@@ -0,0 +1,14 @@
+import Link from 'next/link';
+import styled from 'styled-components';
+import Nav from './Nav';
+ 
+
+export default function Header() {
+  return (
+    <>
+     Hi, I'm header.
+      <Nav></Nav>
+      
+    </>
+  );
+}
---
+++ components/Page.js
@@ -0,0 +1,129 @@
+/* eslint-disable react/prop-types */
+import styled, { createGlobalStyle } from 'styled-components';
+import Header from './Header';
+import { slide as Menu } from "react-burger-menu";
+
+
+const GlobalStyles = createGlobalStyle`
+/* Position and sizing of burger button */
+.bm-burger-button {
+  position: fixed;
+  width: 36px;
+  height: 30px;
+  left: 36px;
+  top: 36px;
+}
+
+/* Color/shape of burger icon bars */
+.bm-burger-bars {
+  background: #373a47;
+}
+
+/* Color/shape of burger icon bars on hover*/
+.bm-burger-bars-hover {
+  background: #a90000;
+}
+
+/* Position and sizing of clickable cross button */
+.bm-cross-button {
+  height: 24px;
+  width: 24px;
+}
+
+/* Color/shape of close button cross */
+.bm-cross {
+  background: #bdc3c7;
+}
+
+/*
+Sidebar wrapper styles
+Note: Beware of modifying this element as it can break the animations - you should not need to touch it in most cases
+*/
+.bm-menu-wrap {
+  position: fixed;
+  height: 100%;
+}
+
+/* General sidebar styles */
+.bm-menu {
+  background: #373a47;
+  padding: 2.5em 1.5em 0;
+  font-size: 1.15em;
+}
+
+/* Morph shape necessary with bubble or elastic */
+.bm-morph-shape {
+  fill: #373a47;
+}
+
+/* Wrapper for item list */
+.bm-item-list {
+  color: #b8b7ad;
+  padding: 0.8em;
+}
+
+/* Individual item */
+.bm-item {
+  display: inline-block;
+}
+
+/* Styling of overlay */
+.bm-overlay {
+  background: rgba(0, 0, 0, 0.3);
+}
+ 
+  html {
+    --red: #ff0000; 
+    --black: #393939;
+    --grey: #3a3a3a;
+    --gray: var(--grey);
+    --lightGrey: #e1e1e1;
+    --lightGray: var(--lightGrey)
+    --offWhite: #ededed;
+    --maxWidth: 1000px;
+    --bs: 0 12px 24px 0 rgba(0,0,0,0.09);
+
+
+  }
+  body {
+    font-family: 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    padding: 0;
+    margin: 0;
+    font-size: 1.5rem;
+    line-height: 2;
+
+  }
+  a {text-decoration:none;color: var(---black)}
+  *, *:before, *:after{
+    box-sizing: inherit;
+  }
+`;
+
+const InnerStyles = styled.div`
+  max-width: var(--maxWidth);
+  margin: 0 auto;
+  padding: 2rem;
+`;
+
+export default function Page({ children }) {
+  return (
+    <div>
+      <GlobalStyles />
+
+      <Menu>
+        <a id="home" className="menu-item" href="/">
+          Home
+        </a>
+        <a id="about" className="menu-item" href="/about">
+          About
+        </a>
+        <a id="contact" className="menu-item" href="/contact">
+          Contact
+        </a>
+        
+      </Menu>
+      <Header />
+      <InnerStyles>{children}</InnerStyles>
+    </div>
+  );
+}
