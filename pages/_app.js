41c885db74ea8979c4be033e077683b92c4857c9
bcoffield <empty@example.com>
2022-02-14 10:02:14 -0700

    Trying umami analytics from test site

--- pages/_app.js
+++ pages/_app.js
@@ -13,6 +13,12 @@ export default function App({ Component, pageProps }) {
           name="viewport"
           content="width=device-width, initial-scale=1.0"
         ></meta>
+        <script
+          async
+          defer
+          data-website-id="969cd4f0-51dc-44f7-890a-0c9f4a0db280"
+          src="https://umami-bcoffield.herokuapp.com/umami.js"
+        ></script>
         <link rel="stylesheet" href="https://use.typekit.net/hmi2ogn.css" />
       </Head>
       <Page>
