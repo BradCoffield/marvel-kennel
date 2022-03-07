a250918ed80733360459c0eeb998d7a8d1e56da2
Brad Coffield <bcoffield@gmail.com>
2022-03-07 11:01:49 -0700

    some content to teaching

--- pages/teaching.js
+++ pages/teaching.js
@@ -1,10 +1,49 @@
 import PageHeading from "../components/PageHeading";
 export default function Teaching() {
   return (
-    <PageHeading
-      content={"Teaching"}
-   
-      image={"/assets/52VintageSunbursts/Black/PNG/Black-18.png"}
-    />
+    <>
+      <PageHeading
+        content={"Teaching"}
+        image={"/assets/52VintageSunbursts/Black/PNG/Black-18.png"}
+      />
+      <div>
+        <p>
+          At Rocky Mountain College, Ashley teaches a wide range of creative
+          writing courses across genres and levels, from Introduction to
+          Creative Writing and Introductory Poetry Workshop to Advanced Fiction
+          Writing and Senior Capstone. As an instructor, she seeks to encourage
+          her students’ intellectual curiosity and willingness to take risks and
+          to create a space where all members of the classroom community feel
+          supported as they engage in the process of making meaning and
+          discovering their voices.
+        </p>
+        <p>
+          Ashley serves as faculty advisor for The Rocky Mountain Review student
+          journal, now in its fourth year. She also runs The Reading Series @
+          RMC, which brings local and regional writers to campus; past events
+          have featured Lowell Jaeger, David Abrams, and Tami Haaland.
+          Additionally, Ashley hosts the Writers-in-Progress reading series,
+          during which creative writing capstone students present their work to
+          the public, and her students have published work from her courses in
+          Montana Quarterly, Allegheny Review, Pithead Chapel, and numerous
+          other venues.
+        </p>
+        <p>
+          In addition to her work at the college level, Ashley has considerable
+          experience teaching in non-traditional settings. In 2016 she
+          co-founded a creative writing group for student veterans and service
+          members at Duquesne University, the DU Dog Tags Literary Society, and
+          in 2017, she received grants from the Pennsylvania Council on the Arts
+          for writing groups she initiated at a local vet center and public
+          library. Beyond this, she has worked with at-risk youth in a group
+          home setting and taught fiction and poetry writing in a summer camp
+          for middle and high school students.
+        </p>
+        <p>
+          For additional information about Ashley’s teaching, including her
+          professional activities and awards, please see her CV.
+        </p>
+      </div>
+    </>
   );
 }
